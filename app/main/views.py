61058d7eeaffb5bcd6ea87efc151f7e6dcf46eb0
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-11 00:11:59 +0300

    feat: configure models- Adds configurations to help model work with extension

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 @main.route('/')
 
 def index():
   
     return render_template('index.html')
+
+@main.route('/megaminds')
+@login_required
+def megaminds():
+    return render_template('megaminds.html')
