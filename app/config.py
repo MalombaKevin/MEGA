96f03ef2fed1e93d2e2faba5e0c74c88179a09d8
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-05-11 07:54:02 +0300

    feat:file upload-adds profile picture update feature

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = "megaminds"
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:megamind@localhost/mega'
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 class ProdConfig(Config):
     pass
 
